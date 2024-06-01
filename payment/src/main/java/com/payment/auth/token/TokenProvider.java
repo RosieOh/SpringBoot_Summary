6f3121192c0a7d7c82acb628b48b52b3154985c3
RosieOh <20172207@gm.hannam.ac.kr>
2024-06-01 18:27:31 +0900

    ✨FEAT : 토큰 기반 인증 방식 로직 구현

--- payment/src/main/java/com/payment/auth/token/TokenProvider.java
+++ payment/src/main/java/com/payment/auth/token/TokenProvider.java
@@ -1,6 +1,13 @@
 package com.payment.auth.token;
 
+import com.payment.member.constant.MemberRole;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
 import javax.crypto.SecretKey;
+import java.util.Date;
+
+import static java.lang.System.currentTimeMillis;
 
 public class TokenProvider {
 
@@ -12,7 +19,38 @@ public class TokenProvider {
         this.tokenExpirationPeriod = tokenExpirationPeriod;
     }
 
-    public String createToken(final Long memberId, final MemberRole role)
+    public String createToken(final Long memberId, final MemberRole role) {
+        final Claims claim = createClaims(memberId, role);
+        return Jwts.builder()
+                .claims(claim)
+                .signWith(secretKey)
+                .compact();
+    }
+
+    private Claims createClaims(final Long memberId, final MemberRole role) {
+        return Jwts.claims()
+                .subject(memberId.toString())
+                .issuedAt(new Date(currentTimeMillis()))
+                .expiration(createExpirationDate())
+                .add("role", role.name())
+                .build();
+    }
+
+    private Date createExpirationDate() {
+        return generateTokenExpiration();
+    }
+
+    private Date generateTokenExpiration() {
+        return new Date(new Date().getTime() + tokenExpirationPeriod);
+    }
+
+    public Claims getTokenClaims(final String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
 
 
 }
